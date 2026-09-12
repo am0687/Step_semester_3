fea2a6dc46816d2cc8f40ba2c4abc3c208868fa5
Aditi Muskan <am0687@srmist.edu.in>
2026-09-12 16:12:49 +0530

    Add Week 4 assignments

---
+++ Week 4/Assignments/ProductArray.java
@@ -0,0 +1,21 @@
+package week4_Assignment;
+
+import java.util.Arrays;
+
+public class ProductArray {
+	public static int[] productExceptSelf(int[] nums)
+	{
+		int prod = 1;
+		int []answer = new int[num.length];
+		for(int i=0; i<nums.length; i++)
+		{
+			prod
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
